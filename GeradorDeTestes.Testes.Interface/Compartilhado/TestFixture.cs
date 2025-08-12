22528b6cd1407349dde82e43fe246d9b8503273f
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-08-12 16:17:22 -0300

    Implementado teste de interface do modulo Disciplina

---
+++ GeradorDeTestes.Testes.Interface/Compartilhado/TestFixture.cs
@@ -0,0 +1,58 @@
+﻿using GeradorDeTestes.Infraestrutura.Orm.Compartilhado;
+using GeradorDeTestes.Testes.Integracao.Compartilhado;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace GeradorDeTestes.Testes.Interface.Compartilhado;
+
+[TestClass]
+public abstract class TestFixture
+{
+    protected static IWebDriver? driver;
+    protected static GeradorDeTestesDbContext? dbContext;
+
+    protected static string enderecoBase = "https://localhost:7098";
+    private static string connectionString = "Server=localhost;Port=5432;Database=GeradorDeTestesDb;UserId=postgres;Password=YourStrongPassword;";
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        dbContext = GeradorDeTestesDbContextFactory.CriarDbContext(connectionString);
+
+        ConfigurarTabelas(dbContext);
+
+        InicializarDriver();
+    }
+
+    [TestCleanup]
+    public void FinalizarTestes()
+    {
+        FinalizarDriver();
+    }
+
+    private static void InicializarDriver()
+    {
+        var options = new FirefoxOptions();
+        options.AddArgument("--headless");
+
+        driver = new FirefoxDriver();
+    }
+
+    private static void FinalizarDriver()
+    {
+        driver?.Quit();
+        driver?.Dispose();
+    }
+
+    private static void ConfigurarTabelas(GeradorDeTestesDbContext dbContext)
+    {
+        dbContext.Database.EnsureCreated();
+
+        dbContext.Testes.RemoveRange(dbContext.Testes);
+        dbContext.Questoes.RemoveRange(dbContext.Questoes);
+        dbContext.Materias.RemoveRange(dbContext.Materias);
+        dbContext.Disciplinas.RemoveRange(dbContext.Disciplinas);
+
+        dbContext.SaveChanges();
+    }
+}
